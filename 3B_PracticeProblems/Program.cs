98ae661c66c688391a7a3e0d5ab0e62b929bc438
hareesh <hareesh.vanapalli.888@gmail.com>
2023-09-30 12:57:05 +0530

    [hari]

---
+++ 3B_PracticeProblems/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace _3B_PracticeProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Day 3B Practice Problems");
+            bool continueExecution = true;
+            while (continueExecution)
+            {
+                Console.WriteLine("Please choose any one of the following program:");
+                Console.WriteLine("1.Types of Constructor \n2.Interface \n3.AccessModifiers \n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        // Using the default constructor
+                        Person person1 = new Person();
+                        Console.WriteLine("Default Constructor:");
+                        person1.Display();
+
+                        // Using the parameterized constructor
+                        Person person2 = new Person("Ram", 25);
+                        Console.WriteLine("\nParameterized Constructor:");
+                        person2.Display();
+
+                        // Using the copy constructor
+                        Person person3 = new Person(person2);
+                        Console.WriteLine("\nCopy Constructor:");
+                        person3.Display();
+                        break;
+                    case 2:
+                        Circle circle = new Circle(5);
+                        Rectangle rectangle = new Rectangle(4, 6);
+
+                        Console.WriteLine($"Circle - Area: {circle.CalculateArea()}, Perimeter: {circle.CalculatePerimeter()}");
+                        Console.WriteLine($"Rectangle - Area: {rectangle.CalculateArea()}, Perimeter: {rectangle.CalculatePerimeter()}");
+                        break;
+                    case 3:
+                        MyBaseClass baseObj = new MyBaseClass();
+                        Console.WriteLine($"PublicField from Main: {baseObj.PublicField}");
+                        // PrivateField is not accessible from Main
+                        // ProtectedField is not accessible from Main
+                        Console.WriteLine($"InternalField from Main: {baseObj.InternalField}");
+
+                        // PublicMethod is accessible from Main
+                        baseObj.PublicMethod();
+                        // PrivateMethod is not accessible from Main
+                        // ProtectedMethod is not accessible from Main
+                        baseObj.InternalMethod();
+
+                        Console.WriteLine("\nDerived class:");
+                        MyDerivedClass derivedObj = new MyDerivedClass();
+                        derivedObj.AccessBaseClassMembers();
+                        break;
+
+
+
+
+                    default:
+                        Console.WriteLine("Invalid option, Please select a valid program.");
+                        break;
+                }
+
+                Console.WriteLine("Do you want to continue?(yes or no)");
+                string userInput = Console.ReadLine().ToLower();
+                if (userInput != "yes")
+                {
+                    continueExecution = false;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
